c8b43eaa6a2e57b2e2f5f82c827d23b0eae437d8
Vlad <Klisunov.vladdi94@gmail.com>
2024-02-08 16:29:49 +0300

    ...

--- Arduino-ESP32/MA41_tester_v1/MA41_tester_v1.ino
+++ Arduino-ESP32/MA41_tester_v1/MA41_tester_v1.ino
@@ -1,7 +1,12 @@
+#define OLED_DISPLAY_TYPE 96
 #include <Oled.h>
 #include <Ble.h>
 #include <AbleButtons.h>
-const gpio_num_t btn_pin=GPIO_NUM_19; //иначе не работает выход из спящего режима, если просто int 19 указать
+const gpio_num_t btn_pin=GPIO_NUM_19; //кнопка энкодера. иначе не работает выход из спящего режима, если просто int 19 указать
+const int sw_pin=18;
+const int uart1_rx_pin=32;
+const int uart1_tx_pin=33;
+const int i_meas_pin=36;
 
 Oled oled;
 Ble ble;
@@ -14,16 +19,20 @@ void buttonableCallback(Button::CALLBACK_EVENT, uint8_t);
 Button btn(int(btn_pin), buttonableCallback); // The button to check.
 
 void setup() { 
-  Serial.begin(115200);   
+  Serial.begin(115200); 
+  Serial1.begin(115200, SERIAL_8N1, uart1_rx_pin, uart1_tx_pin);//* UART1  -> Serial1 //RX Pin //TX Pin //Внешний МА41, который нужно проверить
+  Serial2.begin(115200, SERIAL_8N1); //Внутренний МА41
   ble.begin();
   oled.begin();
   pinMode(btn_pin,INPUT_PULLUP);
-  btn.begin();
+  pinMode(sw_pin,INPUT_PULLUP);
+  pinMode(i_meas_pin,INPUT);
+  btn.begin();  
 }
 
 void loop() {
   if(ble.recvd()!=""){
-    oled.print(1,ble.recvd());
+    oled.prints(ble.recvd());
     ble.clr();
   }
   oled.update();  
@@ -32,8 +41,9 @@ void loop() {
 }
 
 void buttonableCallback(Button::CALLBACK_EVENT event, uint8_t id) {
-  if(event == Button::PRESSED_EVENT) {
-    Serial.println("button pressed");
-    ble.send("test");        
+  if(event == Button::PRESSED_EVENT) {    
+    static int msg_iter=0;
+    msg_iter++;
+    ble.send("test"+String(msg_iter));        
   } 
 }
